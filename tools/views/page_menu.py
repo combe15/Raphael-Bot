21dece0833952507a3a72f5e095ba66abfcbdd14
stalker <superstalkerX@gmail.com>
2021-08-09 21:36:14 -0500

    Added documentation to page_menu

--- tools/views/page_menu.py
+++ tools/views/page_menu.py
@@ -7,9 +7,20 @@ log = logging.getLogger(__name__)
 
 
 class PageMenu(discord.ui.View):
+    """
+    Args:
+        authorized_user (Union[discord.User, discord.Member], optional): [Restricts button interaction to user].
+            Defaults to None.
+        timeout (Optional[float]):
+            [Timeout in seconds from last interaction with the UI before no longer accepting input.]
+            Default is 120 seconds.
+            If None then there is no timeout.
+    """
+
     def __init__(
         self, authorized_user: Union[discord.User, discord.Member] = None, **kwargs
     ):
+
         super().__init__(**kwargs)
         self.authorized_user = authorized_user
         self.value: str = None
